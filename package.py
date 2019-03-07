bb4115e19a0b5435c5a642de616876784f72c9e4
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-03-07 15:18:57 +0000

    Update package.py

--- package.py
+++ package.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-versionHandle = open("../govuk-frontend/package/dist/VERSION.txt")
+versionHandle = open("../govuk-frontend/dist/VERSION.txt")
 govukFrontendVersion = versionHandle.read().strip()
 versionHandle.close()
 print(govukFrontendVersion)
